04ac1680895afd7508931f296844ffbbc2b17f41
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-23 00:02:44 -0400

    added auth with google and pushed user id to firebase

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -7,6 +7,7 @@ import Qs from 'qs';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+//comicVine Api Key
 const apiKey = '9ae979acd25cd191fdc36c5a39ff47c355199161';
 
 //API call
@@ -15,37 +16,120 @@ const apiKey = '9ae979acd25cd191fdc36c5a39ff47c355199161';
 //     console.log(res.data);
 //   });
 
-axios({
-  url: "http://proxy.hackeryou.com",
-  method: "GET",
-  dataResponse: "json",
-  paramsSerializer: function (params) {
-    return Qs.stringify(params, { arrayFormat: 'brackets' })
-  },
-  params: {
-    reqUrl: `http://www.comicvine.com/api/issues`,
-    params: {
-      api_key: apiKey,
-      format: 'json'
-    },
-    proxyHeaders: {
-      'headers_params': 'value'
-    },
-    xmlToJSON: false
-  }
-}).then((res) => {
-  console.log(res.data);
-});
 
 class App extends React.Component {
   constructor(){
     super();
-    
+    this.state = {
+      loggedIn: false,
+      userId: null,
+    }
+    this.loginWithGoogle = this.loginWithGoogle.bind(this);
+    this.logout = this.logout.bind(this);
   }
+
+  componentWillMount(){
+    //----------
+    // API call
+    //----------
+    axios({
+      url: "http://proxy.hackeryou.com",
+      method: "GET",
+      dataResponse: "json",
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: `http://www.comicvine.com/api/issues`,
+        params: {
+          api_key: apiKey,
+          format: 'json'
+        },
+        proxyHeaders: {
+          'headers_params': 'value'
+        },
+        xmlToJSON: false
+      }
+    }).then((res) => {
+      console.log(res.data);
+    });
+
+    //----------------
+    // Authentication
+    //----------------
+    this.dbRef = firebase.database().ref('users/');
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user !== null){
+        this.dbRef.on('value', (snapshot) => {
+          console.log(snapshot.val());
+        })
+        this.setState({
+          loggedIn: true
+        })
+      }else{
+        this.setState({
+          loggedIn: false
+        })
+      }
+    })
+  }
+
+  loginWithGoogle(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then((user) => {
+        console.log(user.user);
+        
+        const firebaseUid = user.user.uid;
+        const firebaseName = user.user.displayName;
+        const firebaseImg = user.user.photoURL;
+        this.setState({
+          userId: firebaseUid,
+          userName: firebaseName,
+          userImg: firebaseImg,
+        },() => {
+          console.log('pushing', this.state.userId);
+          const userInfo = {
+            userName: this.state.userName,
+            userImg: this.state.userImg,
+          }
+          firebase.database().ref('users/' + this.state.userId).push(userInfo);
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  logout(){
+    firebase.auth().signOut();
+    this.dbRef.off('value');
+    console.log('signed out');
+  }
+
+  writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture: imageUrl
+  });
+}
+
+
 
   render() {
     return (
-      <h1>hi</h1>
+      <div>
+
+        {this.state.loggedIn === false && <button onClick={this.loginWithGoogle}>Login with Google</button>
+        }
+        {this.state.loggedIn === true && 
+          <div>
+            <button onClick={this.logout}>Logout</button>
+            <h1>Hello {this.state.username} </h1>
+          </div>}
+      </div>
     )
   }
 }
--- dev/scripts/firebase/firebase-config.js
+++ dev/scripts/firebase/firebase-config.js
@@ -1,3 +1,8 @@
 export const firebaseConfig = {
-        //private firebase config info
+        apiKey: "AIzaSyCA8dHOl37z4m339pWIohCI2I66fusE284",
+        authDomain: "comicproject-b8126.firebaseapp.com",
+        databaseURL: "https://comicproject-b8126.firebaseio.com",
+        projectId: "comicproject-b8126",
+        storageBucket: "comicproject-b8126.appspot.com",
+        messagingSenderId: "253501685502"
 }
