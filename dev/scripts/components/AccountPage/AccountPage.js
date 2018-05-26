99c4b427a1db62d080e6989cf628501da3a70309
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-26 17:40:14 -0400

    fixing auth

--- dev/scripts/components/AccountPage/AccountPage.js
+++ dev/scripts/components/AccountPage/AccountPage.js
@@ -19,8 +19,19 @@ class AccountPage extends React.Component {
       libraryKey: ''
     }
   }
+  componentWillMount(){
+    const user = firebase.auth().currentUser;
 
-
+    if (user != null) {
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+      });
+    }
+  }
 
   componentDidMount(){
     this.setState({
@@ -31,18 +42,28 @@ class AccountPage extends React.Component {
       dbRef.on('value', (snapshot) => {
         const data = snapshot.val();
 
-        //if library is empty
-        if (data.hasOwnProperty('library')){
-          return(
-              this.setState({
-              library: data.library
-            }, () => {
-              console.log(this.state.library)
-            })
-          )
-        }else{
-          return false;
+        const firebaseLibrary = data.library;
+        console.log(data);
+        
+
+        const libraryArrayClone = [];
+
+        for(let issue in firebaseLibrary){
+          console.log(firebaseLibrary[issue].completed );
+          
         }
+        //if library is empty
+        // if (data.hasOwnProperty('library')){
+        //   return(
+        //       this.setState({
+        //       library: data.library
+        //     }, () => {
+        //       console.log(this.state.library)
+        //     })
+        //   )
+        // }else{
+        //   return false;
+        // }
       })
   })
 
@@ -53,6 +74,7 @@ class AccountPage extends React.Component {
   render() {
     return (
       <div>
+        <h1>Account page</h1>
         <ul>
           {
             // console.log(Object.keys(this.state.library))
