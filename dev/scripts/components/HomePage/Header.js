9687538ac19c77378f161dd257816fa62961fc3f
Naomi Ceppetelli <naomiceppetelli@gmail.com>
2018-05-28 14:08:43 -0400

    began header styling

---
+++ dev/scripts/components/HomePage/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="hero">
+                <h1>Comic Cloud</h1>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header 
