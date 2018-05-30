1053ab4483ddecca6aad4dadffc7b9d8380535b8
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-29 22:48:46 -0400

    just went super extra with the hero

--- dev/scripts/components/HomePage/Header.js
+++ dev/scripts/components/HomePage/Header.js
@@ -4,6 +4,9 @@ const Header = () => {
     return (
         <header>
             <div className="hero"></div>
+            <div className="cloud1"><img src="./public/assets/cloud1.png" alt="cloud1"/></div>
+            <div className="cloud2"><img src="./public/assets/cloud2.png" alt="cloud2"/></div>
+            <div className="cloud3"><img src="./public/assets/cloud3.png" alt="cloud3"/></div>
         </header>
     )
 
